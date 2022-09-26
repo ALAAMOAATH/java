fa566489617cdb18b8d9774dd49ae2e4eca31739
ALAAMOAATH <alaamoaath3@gmail.com>
2022-09-26 09:02:37 +0300

    Initial commit

---
+++ ArrayStar.java
@@ -0,0 +1,64 @@
+package java;
+import java.util.Scanner;
+
+class ArrayStar {
+public static void main(String[] args) throws Exception {
+
+//static array 
+/*String array[][]= {
+{"*","*","*","*","*","*"},
+{"*"," "," "," "," ","*"},
+{"*"," "," "," "," ","*"},
+{"*","*","*","*","*","*"}   };*/
+
+
+try (//dynamic array
+Scanner input = new Scanner(System.in)) {
+    System.out.println("enter the row : ");
+    int i=input.nextInt();
+    System.out.println("enter the colum : ");
+    int j=input.nextInt();
+    
+    String array[][]=new String[i][j];
+    for (int n=0 ; n<i ;n++ ){
+        for (int k=0 ; k<j ;k++ )
+            if (n==0||n==i-1||
+               (!(n==0||n==i-1)&&(k==0||k==j-1)) ){
+                 array[n][k]="*";
+                // System.out.print("*");
+            }else{
+                 array[n][k]=" "; 
+                // System.out.print(" ");
+        }
+        //System.out.println(" ");
+    
+        
+    }
+    
+    System.out.println("in array :\n");
+    for (int n=0 ; n<i ;n++ ){
+        for (int k=0 ; k<j ;k++ ){
+            System.out.print(array[n][k]);   
+    
+    
+        }System.out.println(" ");
+    }
+}
+
+
+
+}
+
+
+ 
+
+// for print static array 
+/*for (int i=0;i<array.length;i++){
+    for(int n=0;n<array[i].length;n++){
+        System.out.print(array[i][n]);
+    }
+    System.out.println(" ");
+}*/
+
+
+} 
---
+++ Hw1.java
@@ -0,0 +1,49 @@
+//package java;
+import java.util.Scanner;
+
+public class Hw1{
+/**
+ * @param args
+ */
+public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    int aCounter =0  ;int bCounter =0  ;int cCounter =0  ;int stop =0 ;
+    
+    while(true){
+        System.out.println("Enter one letter : ");
+        String get = input.next().toUpperCase();
+        char   x   = (get.charAt(0));
+        
+    
+        switch(x){
+            case 'A': 
+            aCounter+=1 ; break;
+            case 'B': 
+            bCounter+=1 ; break;
+            case 'C': 
+            cCounter+=1 ; break;
+            case 'Z':
+            stop = 1    ; break ;
+            default:
+            System.out.println("please enter ('A','B','C')just or 'Z' for stop");
+
+
+        }
+        if (stop==1){
+            break;
+        }
+    }
+    System.out.println("Number of 'A' is : "+ aCounter);
+    System.out.println("Number of 'B' is : "+ bCounter);
+    System.out.println("Number of 'C' is : "+ cCounter);
+    
+
+        
+
+    }
+
+
+
+}
+
+
