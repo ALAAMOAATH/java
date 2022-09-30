21d650c3e0e922441702d26889f206acb29ed262
ALAAMOAATH <alaamoaath3@gmail.com>
2022-09-30 17:37:52 +0300

    triangle(60)

--- triangles.java
+++ triangles.java
@@ -1,8 +1,8 @@
 public class triangles {
     public static void main(String[] args) {
-        
-        //90 triangle right
         int num = 5 ;
+        
+    /*     //90 triangle right
         for (int i=1;i<=num;i++){
 
             for(int j=1;j<=i;j++){
@@ -12,9 +12,9 @@ public class triangles {
             System.out.println("");
         }
 
+*/
 
-
-        //90 triangle left
+     /*    //90 triangle left
         for (int i=1;i<=num;i++){
             for(int r=num;r>=i;r--){        
                 System.out.print(" ");
@@ -25,6 +25,18 @@ public class triangles {
             }
             System.out.println("");
         }
+*/
+//60 triangle 
+for (int i=1;i<=num;i++){
+    for(int r=num;r>=i;r--){        
+        System.out.print(" ");
+        }
+    for(int j=1;j<=i;j++){
+        System.out.print("* ");
+                }
+            
+    System.out.println("");
+}
 
 
 
