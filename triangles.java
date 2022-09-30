41467d4c7ca380cc49ef6d681b645e121f521f00
ALAAMOAATH <alaamoaath3@gmail.com>
2022-09-30 21:06:33 +0300

    names

--- triangles.java
+++ triangles.java
@@ -2,19 +2,18 @@ public class triangles {
     public static void main(String[] args) {
         int num = 5 ;
         
-    /*     //90 triangle right
+     //90 triangle right
         for (int i=1;i<=num;i++){
 
             for(int j=1;j<=i;j++){
                 System.out.print("*");
 
             }
-            System.out.println("");
+    System.out.println(" "); // for new line after every row
         }
+      
 
-*/
-
-     /*    //90 triangle left
+    //90 triangle left
         for (int i=1;i<=num;i++){
             for(int r=num;r>=i;r--){        
                 System.out.print(" ");
@@ -23,10 +22,10 @@ public class triangles {
                 System.out.print("*");
 
             }
-            System.out.println("");
+    System.out.println(" "); // for new line after every row
         }
-*/
-//60 triangle 
+     
+//60 triangle  
 for (int i=1;i<=num;i++){
     for(int r=num;r>=i;r--){        
         System.out.print(" ");
@@ -35,8 +34,31 @@ for (int i=1;i<=num;i++){
         System.out.print(j+" ");
                 }
             
-    System.out.println("");
+    System.out.println(" "); // for new line after every row
 }
+
+//inverse triangle 
+for(int i=num;i>=1;i--){
+    for (int j=1 ;j<=i;j++){
+        System.out.print("* ");
+
+    }
+    System.out.println(" "); // for new line after every row
+}
+
+
+//inverse triangle 
+for(int i=num;i>=1;i--){
+    for (int n=num;n>i;n--){
+        System.out.print(" ");
+    }
+    for (int j=1 ;j<=i;j++){
+        System.out.print("* ");
+
+    }
+    System.out.println(" "); // for new line after every row
+}
+
 
 
 
