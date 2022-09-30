077eb7f669309a03e29489769c5bd833cf92628f
ALAAMOAATH <alaamoaath3@gmail.com>
2022-09-30 17:19:31 +0300

    left&right

--- triangles.java
+++ triangles.java
@@ -1,5 +1,32 @@
 public class triangles {
     public static void main(String[] args) {
         
+        //90 triangle right
+        int num = 5 ;
+        for (int i=1;i<=num;i++){
+
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+
+            }
+            System.out.println("");
+        }
+
+
+
+        //90 triangle left
+        for (int i=1;i<=num;i++){
+            for(int r=num;r>=i;r--){        
+                System.out.print(" ");
+                }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+
+            }
+            System.out.println("");
+        }
+
+
+
     }
 }
